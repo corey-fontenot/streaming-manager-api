b73bc84b66569db1c6e02c9393918d1cb55a54b4
Corey Fontenot <corey.fontenot@gmail.com>
2025-03-07 07:35:13 -0600

    Add error handler

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ import cors from 'cors'
 import auth from './routes/auth.ts'
 import logger from './logger.ts'
 import morganMiddleware from './middlewares/morgan.middleware.ts'
+import errorHandler from './middlewares/errorHandler.middleware.ts'
 
 const app: Application = express()
 
@@ -27,8 +28,18 @@ app.get("/", (req, res) => {
 	res.send(JSON.stringify({ 'greeting': 'Welcome To JWT Authentication' }))
 })
 
+app.get("/boom", async (req, res, next) => {
+	try {
+		throw new Error('boom')
+	} catch (err: unknown) {
+		next(err)
+	}
+})
+
 
 
 app.listen(PORT, async () => {
 	logger.info(`Server started at http://localhost:${PORT}`)
 })
+
+app.use(errorHandler)
---
+++ middlewares/errorhandler.middleware.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express"
+import logger from '../logger.ts'
+
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err)
+  res.status(500).send({ errors: [{ status: 500, message: "Internal Server Error" }] });
+}
+
+export default errorHandler
